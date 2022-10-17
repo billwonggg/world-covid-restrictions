1bce3c951896ba4c8f2bb4a41e90bfb5cc2c9577
Bill Wong <billwong7788@gmail.com>
2022-10-17 12:45:55 +1100

    update theme

--- src/theme.js
+++ src/theme.js
@@ -4,9 +4,6 @@ import { indigo } from "@mui/material/colors";
 export const themeLight = createTheme({
   palette: {
     mode: "light",
-    primary: {
-      main: indigo[200],
-    },
     secondary: {
       main: "#dedac0",
     },
@@ -16,11 +13,11 @@ export const themeLight = createTheme({
 export const themeDark = createTheme({
   palette: {
     mode: "dark",
-    background: {
-      default: "#2f3746",
-    },
     secondary: {
       main: "#463e2f",
+    },
+    background: {
+      default: "#2f3746",
     },
   },
 });
